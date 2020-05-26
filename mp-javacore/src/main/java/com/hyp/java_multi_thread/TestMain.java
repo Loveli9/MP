20fd034fc1e81caa677fa017f093c6f618c9438d
huyapeng <huppert_hyp@foxmail.com>
2020-05-26 10:55:40 +0800

    commit 0526

--- mp-javacore/src/main/java/com/hyp/java_multi_thread/TestMain.java
+++ mp-javacore/src/main/java/com/hyp/java_multi_thread/TestMain.java
@@ -1,4 +1,16 @@
 package com.hyp.java_multi_thread;
 
 public class TestMain {
+
+    public static final String hhh = "cib";
+
+    public static void main(String[] args) {
+        String b = "c";
+        String c = "i";
+        String d = "b";
+        String a = "c"+"i"+"b";
+        System.out.println(a == hhh);
+        System.out.println(b+c+d == hhh);
+    }
+
 }
---
+++ mp-javacore/src/main/java/com/hyp/java_multi_thread/cas_atomic/AtomicStampedReferenceDemo.java
@@ -0,0 +1,24 @@
+package com.hyp.java_multi_thread.cas_atomic;
+
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class AtomicStampedReferenceDemo {
+
+    public static void main(String[] args) {
+
+        String str1 = "aaa";
+        String str2 = "bbb";
+        AtomicStampedReference<String> reference = new AtomicStampedReference<String>(str1,1);
+        reference.compareAndSet(str1,str2,reference.getStamp(),reference.getStamp()+1);
+        System.out.println("reference.getReference() = " + reference.getReference());
+
+        boolean b = reference.attemptStamp(str2, reference.getStamp() + 1);
+        System.out.println("b: "+b);
+        System.out.println("reference.getStamp() = "+reference.getStamp());
+
+        boolean c = reference.weakCompareAndSet(str2,"ccc",4, reference.getStamp()+1);
+        System.out.println("reference.getReference() = "+reference.getReference());
+        System.out.println("c = " + c);
+    }
+
+}
