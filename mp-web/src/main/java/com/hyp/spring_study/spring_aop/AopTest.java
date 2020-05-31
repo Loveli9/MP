cc398e1adcee5a2bea303aabe78f854c51d2e008
huyapeng <huppert_hyp@foxmail.com>
2020-06-01 01:30:59 +0800

    aop commit

--- mp-web/src/main/java/com/hyp/spring_study/spring_aop/AopTest.java
+++ mp-web/src/main/java/com/hyp/spring_study/spring_aop/AopTest.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @Slf4j
-public class HelloAop {
+public class AopTest {
 
     //com.hyp.spring_study.spring_aop.service
     @Pointcut("execution(* com.hyp.spring_study.spring_aop.service.*.*(..))")
@@ -17,19 +17,19 @@ public class HelloAop {
     }
 
     //前置增强
-    @Before("com.hyp.spring_study.spring_aop.HelloAop.pointcut()")
+    @Before("com.hyp.spring_study.spring_aop.AopTest.pointcut()")
     public void before() {
         System.out.println("before...");
     }
 
     //后置增强
-    @After("com.hyp.spring_study.spring_aop.HelloAop.pointcut()")
+    @After("com.hyp.spring_study.spring_aop.AopTest.pointcut()")
     public void after() {
         System.out.println("after...");
     }
 
     //环绕增强
-    @Around("com.hyp.spring_study.spring_aop.HelloAop.pointcut()")
+    @Around("com.hyp.spring_study.spring_aop.AopTest.pointcut()")
     public Object around(ProceedingJoinPoint pjp) {
         long currennt = System.currentTimeMillis();
         Object proceed = null;
